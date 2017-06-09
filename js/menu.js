2c372963f7673335eb30378cb6a73e6fb7a02f37
iago dos santos silva <1524654>
2017-06-09 16:36:45 -0300

    Final

---
+++ js/menu.js
@@ -0,0 +1,24 @@
+function accordion(){
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+      acc[i].onclick = function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight){
+          panel.style.maxHeight = null;
+      } else {
+          panel.style.maxHeight = panel.scrollHeight + "px";
+      }
+  }
+}
+}
+
+function openNav() {
+    document.getElementById("sideMenu").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("sideMenu").style.width = "0";
+}
